4093d81568d9e690dd030ea2818b50fb9efd0e06
papparazzo <pappi-@gmx.de>
2017-09-25 21:21:59 +0200

    initial commit

---
+++ src/config/conf.common.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'user' => 'default',
+        'pwd'  => 'defpwd',
+        'db'   => 'sfw2'
+    ],
+
+    'site' => [
+        'maintenance' => false,
+        'offline'     => false,
+    ],
+
+    'debug' => [
+        'on'          => true,
+        'show'        => true,
+        'title'       => '[DEBUG New]'
+    ],
+
+    'defEMailAddr' => [
+        'addr' => 'noreply@localhost',
+        'name' => 'localhost'
+    ],
+
+    'project' => [
+        'title'          => 'localhost',
+        'eMailWebMaster' => 'webmaster@localhost',
+    ]
+];
