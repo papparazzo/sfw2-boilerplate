db9e47a95933561295066038f5e92b12508d1d95
4paproth <stefan.paproth@4com.de>
2022-09-27 20:07:03 +0200

    typo fixed

--- install/database.sql
+++ install/database.sql
@@ -46,6 +46,6 @@ CREATE TABLE IF NOT EXISTS `{TABLE_PREFIX}_path` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO `{TABLE_PREFIX}_path` (`Id`, `ParentPathId`, `Name`, `ControllerTemplateId`, `JsonData`, `ModificationDate`) VALUES
-(0, NULL, 'home', 1, '[\"home\"]', NULL),
+(0, NULL, 'home', 1, '[\"home\"]', NULL)
 
 COMMIT;
