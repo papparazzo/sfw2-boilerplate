c6c75d366414903d9389e6919d2e2ba4a78fb011
papparazzo <pappi-@gmx.de>
2023-09-11 18:52:21 +0200

    copyright notice added

--- public/index.php
+++ public/index.php
@@ -1,10 +1,27 @@
 <?php
 
+/**
+ *  SFW2 - SimpleFrameWork
+ *
+ *  Copyright (C) 2023  Stefan Paproth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
 use SFW2\Boilerplate\Bootstrap;
-use SFW2\Config\Config;
-use Symfony\Component\Yaml\Yaml;
 
 require __DIR__ . '/../vendor/autoload.php';
-
-$boot = new Bootstrap($_SERVER, $_GET, $_POST, new Config(Yaml::parseFile('../config/config.yaml')));
+$boot = new Bootstrap($_SERVER, $_GET, $_POST, '../config/config.yaml');
 $boot->run();
