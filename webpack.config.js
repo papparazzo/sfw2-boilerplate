12f81c14e06f1a86c396ac9417cab62b92b296e6
papparazzo <pappi-@gmx.de>
2023-10-20 06:27:58 +0200

    wip...

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const path = require('path');
-const autoprefixer = require('autoprefixer')
+const autoprefixer = require('autoprefixer');
 const miniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
@@ -22,28 +22,44 @@ module.exports = {
         })
     ],
     module: {
-        rules: [
-            {
-                test: /\.(scss)$/,
-                use: [
-                    {
-                        loader: miniCssExtractPlugin.loader
-                    }, {
-                        loader: 'css-loader'
-                    }, {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    autoprefixer
-                                ]
-                            }
-                        }
-                    }, {
-                        loader: 'sass-loader'
+        rules: [{
+            test: /\.(scss)$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: miniCssExtractPlugin.loader
+            }, {
+                loader: 'css-loader'
+            }, {
+                loader: 'postcss-loader',
+                options: {
+                    postcssOptions: {
+                        plugins: [
+                            autoprefixer
+                        ]
                     }
-                ]
+                }
+            }, {
+                loader: 'sass-loader'
+            }]
+        }, {
+            test: /\.woff2?$/,
+            type: "asset/resource",
+            generator: {
+                filename: '../fonts/[hash][ext][query]'
             }
-        ]
+        }, {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+            generator: {
+                filename: '../img/layout/[hash][ext][query]'
+            }
+        }, {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
     }
 }
